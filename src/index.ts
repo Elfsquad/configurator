f057fc372fac86b33ac765604f09d817ba11cb04
Stan van Rooy <stan@rooy.dev>
2021-09-03 15:12:10 +0200

    feat: export models

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export { ConfiguratorContext } from './configurator/ConfiguratorContext';
 export { IConfiguratorOptions } from './configurator/IConfiguratorOptions';
+export { ConfigurationModels, ConfigurationModel, ConfigurationModelCategory } from './models/ConfigurationModels'
+export { ConflictType, ConfigurationConflict, Configuration, ConfigurationFeature, ConfigurationRequirement, ConfigurationStep, ConfigurationValue, CameraPosition, LinkedConfiguration, LinkedConfigurationModel, FeatureType, FeatureModelRelationshipTypes, FeatureModelRelationshipDisplayType, Mapped3dItems } from './models/Configuration'
+export { Layout3d, MaterialType, Material } from './models/Layout3d'
+export { AfterOrderText, ShowroomFeatureModelSettings, WelcomePageText, Settings } from './models/Settings'
